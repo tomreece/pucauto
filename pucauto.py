b4796ba52e6b9471350ae22413d9e7587908f128
Tom Reece <tomreece@gmail.com>
2015-12-06 21:17:42 -0600

    Bump version to 0.4.2

--- pucauto.py
+++ pucauto.py
@@ -31,7 +31,7 @@ def print_pucauto():
     |    ___||       ||      _||       ||       |  |   |  |  |_|  |
     |   |    |       ||     |_ |   _   ||       |  |   |  |       |
     |___|    |_______||_______||__| |__||_______|  |___|  |_______|
-    pucauto.com                                              v0.4.1
+    pucauto.com                                              v0.4.2
     github.com/tomreece/pucauto
     @pucautobot on Twitter
 
